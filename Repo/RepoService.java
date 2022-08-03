8faa416119686ac6ef5773c6354dcc180dd72ddd
Khuong <s3924577@rmit.edu.vn>
2022-08-03 13:32:36 +0700

    updateRepoService

--- Repo/RepoService.java
+++ Repo/RepoService.java
@@ -1,4 +1,16 @@
 package Repo;
+import java.io.*;
 
 public class RepoService {
+    public static BufferedReader readFile(String path) throws IOException {
+        BufferedReader temp = new BufferedReader(new FileReader(path));
+        temp.close();
+        return temp;
+    }
+    public static void writeFile(String path, String data) throws IOException {
+        BufferedWriter temp = new BufferedWriter(new FileWriter(path, true));
+        temp.newLine();
+        temp.write(data);
+        temp.close();
+    }
 }
