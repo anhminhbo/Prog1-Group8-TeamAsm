40e752b87cb2022deb8528f4e3a3bc9eec36fa8c
Minh Nguyens <wormscott12397@gmail.com>
2022-09-06 18:17:03 +0700

    delete unusued class AdminService and Role

--- constant/Role.java
+++
@@ -1,7 +0,0 @@
-package constant;
-
-public class Role {
-    // Properties for Roles of user (user configurable)
-    public static final String Admin = "Admin";
-    public static final String Member = "Member";
-}
--- user/AdminService.java
+++
@@ -1,7 +0,0 @@
-package user;
-
-public class AdminService {
-    private String adminName;
-
-    public static void main(String[] args) {}
-}
