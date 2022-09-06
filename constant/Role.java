80d6e21c44e790e3027cb944e1ff5275b36aea2c
Minh Nguyens <wormscott12397@gmail.com>
2022-09-06 18:13:43 +0700

    rename admin and member constant

--- constant/Role.java
+++ constant/Role.java
@@ -2,6 +2,6 @@ package constant;
 
 public class Role {
     // Properties for Roles of user (user configurable)
-    public static final String ADMIN = "ADMIN";
-    public static final String MEMBER = "MEMBER";
+    public static final String Admin = "Admin";
+    public static final String Member = "Member";
 }
